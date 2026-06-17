67a9d5fbb5f1b485494103a035d011d517098d23
Jose Torres <jonasapps4401@gmail.com>
2026-06-17 00:12:43 -0500

    feat: initial project setup with Spring Boot, PostreSQL, Docker, Flyway

---
+++ src/test/java/com/homebase/homebase/HomeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homebase.homebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
