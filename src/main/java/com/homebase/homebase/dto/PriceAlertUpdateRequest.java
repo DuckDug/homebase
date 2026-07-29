e9e6994c466c28ff47450250c8233bb03cb99158
Jose Torres <josejonastorres4401@gmail.com>
2026-07-29 13:55:41 -0500

    feat: add at-least-one-field validation to PriceAlertUpdateRequest

--- src/main/java/com/homebase/homebase/dto/PriceAlertUpdateRequest.java
+++ src/main/java/com/homebase/homebase/dto/PriceAlertUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.homebase.homebase.dto;
 
 import com.homebase.homebase.model.PriceAlertCondition;
+import jakarta.validation.constraints.AssertTrue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -12,4 +13,9 @@ public class PriceAlertUpdateRequest {
 
     private BigDecimal targetPrice;
     private PriceAlertCondition condition;
+
+    @AssertTrue(message = "At least one field must be provided")
+    private boolean isAtLeastOneFieldPresent() {
+        return targetPrice != null || condition != null;
+    }
 }
